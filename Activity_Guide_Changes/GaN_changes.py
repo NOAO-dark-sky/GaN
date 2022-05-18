2ef85e48a952799b2289d9cf686ac480842ac124
Marco moreno <marco.moreno.mt@correo.usa.edu.co>
2022-05-17 19:14:38 -0500

    Get Dates of Constellations from the MS EXcel File
    See: #2

--- Activity_Guide_Changes/GaN_changes.py
+++ Activity_Guide_Changes/GaN_changes.py
@@ -6,8 +6,24 @@ import mtranslate, googletrans, os, time
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Pt, Inches
-import pandas
+import pandas as pd
 from shutil import rmtree
+
+# Get Data from the Excel File using Pandas
+dfnorth = pd.read_excel (r'C:\Users\Marco Moreno\OneDrive\Documentos\Enciso Systems\GaN\GaN\Activity_Guide_Changes\GaN_cons_and_dates.xlsx',sheet_name='North')
+dfsouth = pd.read_excel (r'C:\Users\Marco Moreno\OneDrive\Documentos\Enciso Systems\GaN\GaN\Activity_Guide_Changes\GaN_cons_and_dates.xlsx',sheet_name='South')
+
+#Get Dates From North and South Constellations
+NorthConsUser = str(input("Please enter the name of the North Constellation: "))
+NorthDateUser=dfnorth.loc[dfnorth['Constellations'] == NorthConsUser, 'Dates'].iloc[0]
+print(NorthDateUser)
+
+SouthConsUser = str(input("Please enter the name of the South Constellation: "))
+SouthDateUser=dfsouth.loc[dfsouth['Constellations'] == SouthConsUser, 'Dates'].iloc[0]
+print(SouthDateUser)
+
+
+
 
 
 
