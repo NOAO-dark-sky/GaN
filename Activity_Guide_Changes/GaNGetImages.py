7929ee0c76aaa44451a721e08cb256692e1eaf43
Marco moreno <marco.moreno.mt@correo.usa.edu.co>
2022-07-03 09:39:59 -0500

    Getting the images from all constellations and all latitudes succesfully Note: Taurus and Gemini don't have images in the webpage, for that reason those constellations won't be in the constellations lists
    See: #8

--- Activity_Guide_Changes/GaNGetImages.py
+++ Activity_Guide_Changes/GaNGetImages.py
@@ -3,6 +3,7 @@ import requests
 from IPython.display import Image, display
 import os
 from shutil import rmtree
+import re
 
 # Create a new folder to download the images
 def createImageDir():
@@ -13,26 +14,21 @@ def createImageDir():
     return savePath
 
 
-# Create an unique constellation list from the user
-def getContellationList(constNorth, ConstSouth):
-    constList1 = constNorth
-    constList2 = ConstSouth
-
-    for cons in constList2:
-        if cons in constList1:
-            pass
-        else:
-            constList1.append(cons)
-
-    return constList1
+# Get the latitudes according to the images names in the GaN webpage
+def transformLatitude(lat):
+    if "N" in lat:
+        lat = str(lat.rstrip(lat[-1]))
+    else:
+        lat = str(lat.lower())
+    return lat        
 
 
 # Downlaod the images and save them in the images folder
-def imageDownload():
+def imageDownload(constNorth, constSouth, latNorth, latSouth):
     
     os.chdir(createImageDir())
 
-    # Get the url in GaN website where the images are located
+        # Get the url in GaN website where the images are located
     url = 'https://www.globeatnight.org/magcharts'
     gan = requests.get(url)
     #Verify the connection
@@ -41,19 +37,33 @@ def imageDownload():
     # get the soup to pass the content
     soup = BeautifulSoup(gan.text, 'html.parser')
     # Searching the "div with id = finder" where are the images links
-    magfloat = soup.find('div' , attrs= {"id" : "finder"}).find_all('div')
+    image = soup.find('div' , attrs= {"id" : "finder"}).find('img')
+    imageLink = str(image['src'])
 
-    images = soup.find_all('img')
+    for const in constNorth:
+        for lat in latNorth:
+            newLink = imageLink.replace("hercules", const.lower()).replace("10", transformLatitude(lat))
 
-
-
-    for image in images:
-        link = image['src']
-        if 'hercules' in link:
-            linkString = str(link.replace('https://www.globeatnight.org/img/2021/', '').replace('/day/600/','-'))
+            linkString = str(newLink.replace('https://www.globeatnight.org/img/2021/', '').replace('/day/600/','-'))
             with open(linkString, 'wb') as f:
-                img = requests.get(link)
+                img = requests.get(newLink)
                 f.write(img.content)
 
 
+    for const in constSouth:
+        for lat in latSouth:
+            newLink = imageLink.replace("hercules", const.lower()).replace("10", transformLatitude(lat))
 
+            linkString = str(newLink.replace('https://www.globeatnight.org/img/2021/', '').replace('/day/600/','-'))
+            with open(linkString, 'wb') as f:
+                img = requests.get(newLink)
+                f.write(img.content)
+
+
+northConstellations = ["Perseus", "Leo", "Bootes", "Cygnus", "Pegasus", "Orion", "Hercules"]
+southConstellations = ["Orion","Canis Major", "Crux", "Leo", "Bootes", "Scorpius", "Hercules", "Sagittarius", "Grus", "Pegasus"]
+
+latitudesNorth = ["50N", "40N", "30N", "20N", "10N", "0"]
+latitudesSouth = ["0", "10S", "20S", "30S", "40S"]
+
+imageDownload(northConstellations, southConstellations, latitudesNorth, latitudesSouth)
